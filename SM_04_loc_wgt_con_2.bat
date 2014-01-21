886130bd5cd596fb2827f4a4c192b90d37481c91
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-01-21 14:06:52 +0200

    Add variant SM_04_loc_wgt_con_2

---
+++ SM_04_loc_wgt_con_2.bat
@@ -0,0 +1,1 @@
+call zig3 -r SM_04_loc_wgt_con_2/SM_04_loc_wgt_con_2.dat SM_04_loc_wgt_con_2/SM_04_loc_wgt_con_2.spp output/SM_04_loc_wgt_con_2/SM_04_loc_wgt_con_2.txt 0 0 1 0 --grid-output-formats compressed-tif
